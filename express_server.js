5bf5562329410c16c4f420888bde9ad021226583
Salma Ibrahim <salma.ibrahim1307@gmail.com>
2022-06-13 21:21:49 +0000

    added more endpoint and added  HTML content

--- express_server.js
+++ express_server.js
@@ -2,11 +2,21 @@ const express = require('express');
 const app = express(); //makes the server/app an object?
 const PORT = 8080
 
+const urlDatabase = {
+  "b2xVn2": "http://www.lighthouselabs.ca",
+  "9sm5xK": "http://www.google.com"
+};
 
 app.get('/', (req,res) => {
-  res.send("welcome!")
-})
+  res.send("Welcome!")
+});
+app.get("/urls.json", (req, res) => {
+  res.json(urlDatabase);
+});
+app.get("/hello", (req, res) => {
+  res.send("<html><body>Hello <b>World</b></body></html>\n")
+});
 
 app.listen(PORT, () => {
   console.log(`app is listening on port ${PORT}`)
-}) 
+});

bf21966d1f8407d62ef9ac04c0cdb8305ed07057
Salma Ibrahim <salma.ibrahim1307@gmail.com>
2022-06-17 00:57:48 +0000

    added login and logout routes

--- express_server.js
+++ express_server.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const app = express(); //makes the server/app an object?
 const PORT = 8080
+const cookieParser = require('cookie-parser')
+app.use(cookieParser())
+
+
 
 app.set("view engine", "ejs");
 
@@ -21,53 +25,78 @@ const urlDatabase = {
 }; 
 
 const bodyParser = require("body-parser");
+const res = require('express/lib/response');
 app.use(bodyParser.urlencoded({extended: true}));
 
 app.get('/', (req,res) => {
-  res.send("Welcome!")
+  res.redirect("/urls")
 });
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
-});
-
-app.get("/urls", (req,res) => {
-  const templateVars = { urls: urlDatabase };
-res.render("urls_index", templateVars) // this route show the urls_index page 
-})
-app.get("/urls/new", (req, res) => { //place this above the next route so it does not mistake /new as an id
-  res.render("urls_new"); //this route renders the submission form urls_new to user
-});
-app.get("/urls/:shortURL", (req, res) => { // routue to display a single URL and its shortened form
-  const longURL = urlDatabase[req.params.shortURL]
-  const templateVars = { shortURL: req.params.shortURL, longURL }; //??
-  res.render("urls_show", templateVars);
-});
-
-app.get("/urls/:id", (req, res) => { // ^^^ the id if the ID of the long url was b2xVn2, then the url would look like /urls/b2xVn2
-  res.render("urls_new")         
-});
-
-app.get("/u/:shortURL", (req, res) => { //this route will direct us to the longURL
-  const longURL = urlDatabase[req.params.shortURL]
-  res.redirect(longURL);
 });
 
 app.post("/urls", (req, res) => {
   const id = generateRandomString()
   const longURL = req.body.longURL
   urlDatabase[id] = longURL /* */
-  res.redirect(`urls/:${id}`)    //
+  res.redirect(`urls/${id}`)    //
+});
+
+app.get("/urls", (req,res) => {
+  const templateVars = { urls: urlDatabase , username: req.cookies["username"]}; //username wasnt define because /new use header.ejs
+res.render("urls_index", templateVars) 
+})
+app.get("/urls/new", (req, res) => { 
+  const templateVars = { username: req.cookies["username"],};
+  res.render("urls_new", templateVars); //this route renders the submission form urls_new to user
+});
+// app.get("/urls/:shortURL", (req, res) => { // routue to display a single URL and its shortened form
+//   const longURL = urlDatabase[req.params.shortURL]
+//   const templateVars = { shortURL: req.params.shortURL, longURL }; //??
+//   res.render("urls_show", templateVars);
+// });
+
+// app.get("/urls/:id", (req, res) => { // ^^^ the id if the ID of the long url was b2xVn2, then the url would look like /urls/b2xVn2
+//   res.render("urls_new")         
+// });
+
+app.get("/u/:shortURL", (req, res) => { //this route will direct us to the longURL
+  const longURL = urlDatabase[req.params.shortURL]
+  res.redirect(longURL);
+});
+
+app.get("/urls/:shortURL", (req,res)=>{
+  const shortURL = req.params.shortURL
+  // console.log(req.params.shortURL)
+  const longURL =  urlDatabase[shortURL]
+  const templateVars = {shortURL, longURL, username: req.cookies["username"]} ;
+  // console.log(templateVars);
+  res.render("urls_show", templateVars)
+})
+
+app.post("/urls/:shortURL", (req, res) => { 
+  const shortURL = req.params.shortURL
+  const longURL = req.body.longURL
+  console.log(shortURL);
+  urlDatabase[shortURL] = longURL 
+  res.redirect("/urls");
 });
 
 app.post("/urls/:shortURL/delete", (req,res) => {
-
   delete urlDatabase[req.params.shortURL]
-
   res.redirect("/urls") //??
 })
 
+app.post("/login", (req,res) => {
+  res.cookie("username", req.body.username) //res.cookie = giving user a cookie mailing id
+  res.redirect("/urls") //!!!
+})
+
+app.post("/logout", (req,res) => {
+  res.clearCookie("username") // delete request cookie, take it back and dipose
+  res.redirect("/urls")
+})
 
 app.listen(PORT, () => {
-
   console.log(`app is listening on port ${PORT}`)
 });

ce87835d0856ec6283f9fa2dfe88366e21922809
Salma Ibrahim <salma.ibrahim1307@gmail.com>
2022-07-01 06:06:26 +0000

    remove hardcoded users and url databases, add easy to read comments,add check to GET /urls/:shortURL, linted

--- express_server.js
+++ express_server.js
@@ -3,13 +3,10 @@ const PORT = 8080;
 const app = express();
 const { generateRandomString, usersURL, getUserEmail } = require("./helpers");
 const bodyParser = require("body-parser");
-const cookieParser = require("cookie-parser");
 const bcrypt = require("bcryptjs");
 const cookieSession = require("cookie-session");
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(cookieParser());
-
 app.use(
   cookieSession({
     name: "session",
@@ -19,31 +16,9 @@ app.use(
 );
 
 const urlDatabase = {};
-// const urlDatabase = {
-//   b6UTxQ: {
-//     longURL: "https://www.tsn.ca",
-//     userID: "aJ48lW",
-//   },
-//   i3BoGr: {
-//     longURL: "https://www.google.ca",
-//     userID: "aJ48lW",
-//   },
-// };
-
 const users = {};
-// const users = {
-//   a1: {
-//     id: "a1",
-//     email: "a@a.com",
-//     password: "123",
-//   },
-//   user2RandomID: {
-//     id: "user2RandomID",
-//     email: "user2@example.com",
-//     password: "dishwasher-funk",
-//   },
-// };
 
+//routing to homepage
 app.get("/", (req, res) => {
   res.redirect("/urls");
 });
@@ -52,6 +27,7 @@ app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
 
+//route to post the URLs
 app.post("/urls", (req, res) => {
   const userID = users[req.session.user_id].id;
   const shortURL = generateRandomString();
@@ -60,6 +36,7 @@ app.post("/urls", (req, res) => {
   res.redirect(`urls/${shortURL}`);
 });
 
+//route to show the list of users tinyURL (renders the index)
 app.get("/urls", (req, res) => {
   let templateVars = { urls: null, user: null };
   if (req.session) {
@@ -69,6 +46,7 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVars);
 });
 
+//route to create new tinyURL
 app.get("/urls/new", (req, res) => {
   const user = users[req.session.user_id];
   if (!user) {
@@ -79,27 +57,26 @@ app.get("/urls/new", (req, res) => {
   }
 });
 
+//this route will redirect you to the longURL
 app.get("/u/:shortURL", (req, res) => {
   const longURL = urlDatabase[req.params.shortURL];
   res.redirect(longURL);
 });
 
+//route to show edit page for logged in user, or redirect a non-logged in user to the login page
 app.get("/urls/:shortURL", (req, res) => {
-  console.log(req.session);
-  console.log("I AM HERE");
   const user = users[req.session.user_id];
   if (user) {
     const shortURL = req.params.shortURL;
     const longURL = urlDatabase[shortURL].longURL;
     const templateVars = { shortURL, longURL, user };
     res.render("urls_show", templateVars);
-    // templateVars.user = users[req.session.user_id];
-    // templateVars.urls = usersURL(req.session.user_id, urlDatabase);
   } else {
     res.redirect("/login");
   }
 });
 
+//Route to edit a longURL
 app.post("/urls/:shortURL", (req, res) => {
   const shortURL = req.params.shortURL;
   const longURL = req.body.longURL;
@@ -107,6 +84,7 @@ app.post("/urls/:shortURL", (req, res) => {
   res.redirect("/urls");
 });
 
+//route for deleting shortURL only by the owner, prints message if some unathorized person tries to delete the users shortURL (for example, in commandline with cURL)
 app.post("/urls/:shortURL/delete", (req, res) => {
   const user = users[req.session.user_id];
   if (user && user.id === urlDatabase[req.params.shortURL].userID) {
@@ -119,12 +97,14 @@ app.post("/urls/:shortURL/delete", (req, res) => {
   }
 });
 
+//route to registration page
 app.get("/register", (req, res) => {
   const user = users[req.session.user_id];
   const templateVars = { user };
   res.render("registration", templateVars);
 });
 
+//route for registration. will add new user in databse with user, email, and passowrd,  also error handling incase email alardy exists.
 app.post("/register", (req, res) => {
   const id = generateRandomString();
   const password = req.body.password;
@@ -146,18 +126,24 @@ app.post("/register", (req, res) => {
   res.redirect("/urls");
 });
 
+//route to login page
 app.get("/login", (req, res) => {
   const user = users[req.session.user_id];
   const templateVars = { user };
   res.render("login", templateVars);
 });
 
+//route for login, checks if email and password match the ones in the users database, also some error handling in case of missing or incorrect credentials
 app.post("/login", (req, res) => {
   const email = req.body.email;
   const password = req.body.password;
   const user = getUserEmail(email, users);
   console.log(user);
-  if (user && user.hashedPassword && bcrypt.compareSync(password, user.hashedPassword)) {
+  if (
+    user &&
+    user.hashedPassword &&
+    bcrypt.compareSync(password, user.hashedPassword)
+  ) {
     req.session.user_id = user.id;
     res.redirect("/urls");
   } else if (email === "") {
@@ -169,11 +155,13 @@ app.post("/login", (req, res) => {
   }
 });
 
+//route for logout, will erase req.session user cookie and redirect to homepage
 app.post("/logout", (req, res) => {
   req.session = null;
   res.redirect("/urls");
 });
 
+//will send message once connection is established.
 app.listen(PORT, () => {
   console.log(`app is listening on port ${PORT}`);
 });
